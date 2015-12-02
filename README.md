71a0fd25bf92447298ccf5901607009a8f3ce810
carlos.baeza <carlos.baeza@idealo.de>
2015-12-02 15:11:44 +0100

    add first controller, spring web and update README

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 ## server
 
-### Start server
+### Start rest server
 
 Start the server via Maven
+> mvn clean package
 
 > mvn -f server/pom.xml spring-boot:run
+
+Call
+> http://localhost:8090/server/
 
 ## webapp
---
+++ server/src/main/java/com/cbaeza/server/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.cbaeza.server.controller;
+
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <br/>
+ *
+ * @author carlos.baeza , carlos.baeza@idealo.de<br/>
+ * @since 02.12.15
+ */
+@RestController
+@RequestMapping( value = "/")
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET, produces = MimeTypeUtils.TEXT_HTML_VALUE)
+    public String sayHello(){
+        return "hello";
+    }
+
+}
