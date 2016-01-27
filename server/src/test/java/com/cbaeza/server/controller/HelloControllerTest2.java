9b2ebc042bcf86ae5c19aa9716aac443ae745e7f
carlos.baeza <carlos.baeza@idealo.de>
2016-01-27 17:28:59 +0100

    improve test - wip

--- server/src/test/java/com/cbaeza/server/controller/HelloControllerTest2.java
+++ server/src/test/java/com/cbaeza/server/controller/HelloControllerTest2.java
@@ -1,13 +1,19 @@
 package com.cbaeza.server.controller;
 
-import com.cbaeza.server.Application;
+import static com.jayway.restassured.RestAssured.given;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.util.MimeTypeUtils;
 
-import static com.jayway.restassured.RestAssured.*;
+import com.cbaeza.server.Application;
+import com.jayway.restassured.builder.RequestSpecBuilder;
+import com.jayway.restassured.specification.RequestSpecification;
 
 /**
  * using RestAssured
@@ -18,13 +24,28 @@ import static com.jayway.restassured.RestAssured.*;
 @WebAppConfiguration
 public class HelloControllerTest2 {
 
-	@Test
-	public void test200(){
-		get("/api").then().assertThat().statusCode(200);
-	}
+    public static final RequestSpecification getRequestSpecification() {
+        RequestSpecification spec = new RequestSpecBuilder()
+                .addHeader(HttpHeaders.ACCEPT, MimeTypeUtils.APPLICATION_JSON_VALUE)
+                .build();
+        return spec;
+    }
 
-	@Test
-	public void test404() {
-		get("/api/id/65454").then().assertThat().statusCode(404);
-	}
+    @Test
+    public void test200() {
+        given(getRequestSpecification())
+                .get("/api/")
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+
+    @Test
+    public void test404() {
+        given(getRequestSpecification())
+                .get("/api/id/65454")
+                .then()
+                .assertThat()
+                .statusCode(404);
+    }
 }
